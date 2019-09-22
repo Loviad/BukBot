423de64567d52dc4f79ecb9449b274b4536a69be
sergey <testmail>
2019-09-23 02:38:35 +0500

    пытаюсь добавить телегу

---
+++ src/test/kotlin/com/example/bukbot/BukBotApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.bukbot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BukBotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
